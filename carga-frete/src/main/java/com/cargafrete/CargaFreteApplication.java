f626f788fb4f97c320e4938fc09e81e48736d407
GUGA NERI <guganeriss@gmail.com>
2019-04-03 11:22:04 -0300

    add

---
+++ carga-frete/src/main/java/com/cargafrete/CargaFreteApplication.java
@@ -0,0 +1,13 @@
+package com.cargafrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargaFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargaFreteApplication.class, args);
+	}
+
+}
---
+++ carga-frete/src/main/java/com/cargafrete/model/Cliente.java
@@ -0,0 +1,5 @@
+package com.cargafrete.model;
+
+public class Cliente {
+
+}
