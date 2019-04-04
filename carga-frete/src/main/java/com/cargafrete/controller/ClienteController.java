7e3432d33536f8319117872b8d73e02f8c2d0d40
Guga Neri <guganeriss@gmail.com>
2019-04-03 22:49:38 -0300

    add

--- carga-frete/src/main/java/com/cargafrete/controller/ClienteController.java
+++ carga-frete/src/main/java/com/cargafrete/controller/ClienteController.java
@@ -1,5 +1,19 @@
 package com.cargafrete.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cargafrete.service.ClienteService;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	
+	
 
 }
--- carga-frete/src/main/java/com/cargafrete/service/ClienteService.java
+++ carga-frete/src/main/java/com/cargafrete/service/ClienteService.java
@@ -1,5 +1,32 @@
 package com.cargafrete.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cargafrete.model.Cliente;
+import com.cargafrete.repository.ClienteRepository;
+
+import antlr.collections.List;
+
+@Service
 public class ClienteService {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente cadCliente(Cliente cliente) {
+		Cliente clienteCad = clienteRepository.save(cliente);
+		return clienteCad;
+	}
+	public List listar(){
+		return (List) clienteRepository.findAll();
+	}
+	public Cliente getCliente(Long id) {
+		return getCliente(id);
+	}
+	
+	public void deletCliente(Long id) {
+		Cliente cliente = getCliente(id);
+		clienteRepository.delete(cliente);
+	}
 }
